b462ea31db6565f3e30e288dfc1cd37ddd797204
pitsomnk <pitsomnk@gmail.com>
2024-01-04 14:26:12 -0800

    another commit

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -5,6 +5,11 @@
 Determines all possible solutions to placing N
 N non-attacking queens on an NxN chessboard.
 
+Example:
+    $ ./101-nqueens.py N
+
+N must be an integer greater than or equal to 4.
+
 Attributes:
     board (list): A list of lists representing the chessboard.
     solutions (list): A list of lists containing solutions.
