7cf8ae47f4bb28747f065d326546fb7594143d83
pitsomnk <pitsomnk@gmail.com>
2024-01-04 14:17:58 -0800

    another commit

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -61,4 +61,6 @@ class Rectangle:
                 for j in range(self.__height)]))
 
     def __repr__(self):
-
+        """ return a string representation of the rectangle
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
